8c08ac5ac2501f69f4e2051be4900b78bda5b435
Christopher David <chris@arcadelabs.co>
2024-01-12 14:25:06 -0600

    Load Extism plugin and call from UI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use extism::*;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let url = Wasm::url(
+        "https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm"
+    );
+    let manifest = Manifest::new([url]);
+    let mut plugin = Plugin::new(&manifest, [], true).unwrap();
+    let res = plugin.call::<&str, &str>("count_vowels", "Hello, world!").unwrap();
+    format!("Hello, {}! Your name has vowels: {}", name, res)
 }
 
 fn main() {
-    tauri::Builder::default()
+    tauri::Builder
+        ::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
